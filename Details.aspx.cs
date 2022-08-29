52aa73e26effc95f7b25379f1c5348e6e58b5628
Salem-Mufarreh <salemmuf9@gmail.com>
2022-08-29 13:00:21 +0300

    added Details

---
+++ Details.aspx.cs
@@ -0,0 +1,43 @@
+﻿using BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WebApplication2.Context;
+
+namespace WebApplication2
+{
+    public partial class Details : System.Web.UI.Page
+    {
+        protected readonly ApplicationContext context = new ApplicationContext();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["LoggedInUser"] == null)
+            {
+                ToastrNotifications toastr = new ToastrNotifications("Please Login First", "error");
+                Session["Message"] = toastr;
+
+                Response.Redirect(nameof(Home));
+            }
+
+            int value = int.Parse(Request.Params["id"]);
+            List<Book> book = context.Books.Where(x => x.Id == value).ToList();
+            txtAuthor.Text = book[0].Author;
+            txtDescription.Text = book[0].Description;
+            txtISBN.Text = book[0].Id.ToString();
+            txtName.Text = book[0].Title;
+            txtPrice.Text = book[0].Price.ToString() + "$";
+            if (book[0].Status.Equals(SD.notAvailable))
+            {
+                btnCart.Visible = false;
+            }
+
+        }
+        protected void btnBack_Click(object sender, EventArgs e)
+        {
+            Response.Redirect(nameof(Home));
+        }
+    }
+}
