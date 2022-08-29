15356d5594c4ebbb78b661f8bc9ecc17bd19add5
Salem-Mufarreh <salemmuf9@gmail.com>
2022-08-29 12:48:16 +0300

    Add project files.

---
+++ Cart.aspx.cs
@@ -0,0 +1,21 @@
+﻿using BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WebApplication2.Context;
+
+namespace WebApplication2
+{
+    public partial class Cart : System.Web.UI.Page
+    {
+        protected readonly ApplicationContext context = new ApplicationContext();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ ListBooks.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication2
+{
+    public partial class ListBooks : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SqlDataSource1.DeleteCommand = "DELETE from Books where Id=@Id";
+            SqlDataSource1.UpdateCommand = "UPDATE Books SET Title=@Title,Author = @Author,Description= @Description,Quantity = @Quantity,Status = @Status, Price = @Price where Id = @Id";
+
+        }
+    }
+}
